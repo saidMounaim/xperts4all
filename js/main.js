9e49c5ee02a270173468cea5b9719ae9855d6b27
Said Mounaim <saidmounaim00@gmail.com>
2019-05-23 11:47:24 +0000

    Update Projects

--- js/main.js
+++ js/main.js
@@ -124,31 +124,31 @@
             $folioItems = $('.item-folio__thumb');
 
         // get items
-        $folioItems.each( function(i) {
+        // $folioItems.each( function(i) {
 
-            var $folio = $('.item-folio'),
-                $thumbLink =  $folio.find('.thumb-link'),
-                $title = $folio.find('.item-folio__title'),
-                $caption = $folio.find('.item-folio__caption'),
-                $titleText = '<h4>' + $.trim($title.html()) + '</h4>',
-                $captionText = $.trim($caption.html()),
-                $href = $thumbLink.attr('href'),
-                $size = $thumbLink.data('size').split('x'),
-                $width  = $size[0],
-                $height = $size[1];
+        //     var $folio = $('.item-folio'),
+        //         $thumbLink =  $folio.find('.thumb-link'),
+        //         $title = $folio.find('.item-folio__title'),
+        //         $caption = $folio.find('.item-folio__caption'),
+        //         $titleText = '<h4>' + $.trim($title.html()) + '</h4>',
+        //         $captionText = $.trim($caption.html()),
+        //         $href = $thumbLink.attr('href'),
+        //         $size = $thumbLink.data('size').split('x'),
+        //         $width  = $size[0],
+        //         $height = $size[1];
         
-            var item = {
-                src  : $href,
-                w    : $width,
-                h    : $height
-            }
+        //     var item = {
+        //         src  : $href,
+        //         w    : $width,
+        //         h    : $height
+        //     }
 
-            if ($caption.length > 0) {
-                item.title = $.trim($titleText + $captionText);
-            }
+        //     if ($caption.length > 0) {
+        //         item.title = $.trim($titleText + $captionText);
+        //     }
 
-            items.push(item);
-        });
+        //     items.push(item);
+        // });
 
         // bind click event
         $folioItems.each(function(i) {
